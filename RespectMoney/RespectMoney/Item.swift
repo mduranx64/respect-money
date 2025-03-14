4d26dacb2bb6307c67d295d8f57a38076baa9d30
Miguel Duran <mduranr@falabella.cl>
2025-03-13 23:16:20 -0300

    Add empty project with swift data configuration

---
+++ RespectMoney/RespectMoney/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RespectMoney
+//
+//  Created by Miguel Duran on 13-03-25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RespectMoney/RespectMoneyTests/RespectMoneyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RespectMoneyTests.swift
+//  RespectMoneyTests
+//
+//  Created by Miguel Duran on 13-03-25.
+//
+
+import Testing
+@testable import RespectMoney
+
+struct RespectMoneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
